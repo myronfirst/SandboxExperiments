2f599973779c8a051a411568fb4a1f8718a0b76e
Myron Tsatsarakis <myronfirst@hotmail.com>
2024-07-28 22:21:47 +0300

    margin of error computation and multiple runs

--- alloc_experiments/RunAll.py
+++ alloc_experiments/RunAll.py
@@ -1,5 +1,8 @@
 import subprocess
 import os
+import numpy as np
+from scipy import stats
+import math
 
 from pathlib import Path
 
@@ -14,14 +17,13 @@ BENCHES = [
     './build/bench_make_persistent_atomic',
     './build/bench_pmem',
 ]
-# run Read operation with LOGN_OPS = '6', then change NUM_OPS=10**6 in Plot.py
 
 OPERATIONS = [
     ['None', 'Alloc'],
     ['Block', 'Read'],
     ['Block', 'Write'],
-    ['Sparse', 'Read'],
-    ['Sparse', 'Write'],
+    # ['Sparse', 'Read'],
+    # ['Sparse', 'Write'],
 ]
 N_THREADS = [
     '1',
@@ -39,44 +41,64 @@ MAX_RUNS = 10
 
 UNIT = 'microseconds'
 
+MAX_RUNS_CASES = []
 
-def RepeatRun(args):
+
+def get_error_margin_percentage(data):
+    mean = np.mean(data)
+    std_dev = np.std(data, ddof=1)  # Use ddof=1 for sample standard deviation
+    n = len(data)
+    standard_error = std_dev / math.sqrt(n)
+    confidence_level = 0.95
+    degrees_freedom = n - 1
+    critical_value = stats.t.ppf((1 + confidence_level) / 2, degrees_freedom)
+    error_margin = critical_value * standard_error
+    error_margin_percentage = error_margin / mean
+    return error_margin_percentage
+
+
+def run_repeatedly(args):
     '''
-    Run up until MAX_RUNS times or until error margin of MIN_RUNS consecutive times is less than 2%
+    Run up until MAX_RUNS times, or until error margin of MIN_RUNS consecutive times is less than 2%
+    Returns average of MAX_RUNS times minus min and max values, or average of consecutive MIN_RUNS times
     '''
-    my_env = os.environ.copy()
-    my_env['VMMALLOC_POOL_DIR'] = '/mnt/pmem0/myrontsa'
-    my_env['VMMALLOC_POOL_SIZE'] = f'{1024 *1024*1024*32}'  # 32GB
-    stats = {'dur': [], 'ops': []}
-    retDurAvg = 0
-    retOpsAvg = 0
-    for i in range(MAX_RUNS):
-        completedProc = subprocess.run(args,
-                                       capture_output=True, env=my_env)
-        lines = completedProc.stdout.splitlines()
-        dur, ops = int(lines[0]), int(lines[1])
-        stats['dur'].append(dur)
-        stats['ops'].append(ops)
-        if i < MIN_RUNS:
-            continue
-        durWindow = [dur for dur in stats['dur'][i-MIN_RUNS:]]
-        durAvg = sum(durWindow) / len(durWindow)
-        errorMargin = sum(durWindow) / len(durWindow)
-        if (errorMargin < 0.02):
-            retDurAvg = durAvg
-            break
-    durWindowAll = [dur for dur in stats['dur']]
-    if retDurAvg == 0:
-        retDurAvg = sum(durWindowAll) / len(durWindowAll)
+    my_env = {}
+    if args[0] == './build/bench_vmmalloc':
+        my_env = os.environ.copy()
+        my_env['VMMALLOC_POOL_DIR'] = '/mnt/pmem0/myrontsa'
+        my_env['VMMALLOC_POOL_SIZE'] = f'{1024 *1024*1024*32}'  # 32GB
+
+    def get_duration_avg():
+        operations = None
+        durations = np.array([], dtype=float)
+        for i in range(MAX_RUNS):
+            completed_proc = subprocess.run(
+                args, capture_output=True, env=my_env)
+            lines = completed_proc.stdout.splitlines()
+            dur, ops = int(lines[0]), int(lines[1])
+            if operations is None:
+                operations = ops
+            durations = np.append(durations, dur)
+            if i < MIN_RUNS:
+                print(f'[{i}]{args},{operations},{dur}')
+                continue
+            window = durations[-MIN_RUNS:]
+            emp = get_error_margin_percentage(window)
+            print(f'[{i}]{args},{operations},{dur},{emp}')
+            if (emp < 0.02):
+                return np.mean(window).astype(int), operations
+        global MAX_RUNS_CASES
+        MAX_RUNS_CASES.append(args)
+        print(f'MAX_RUNS:{args}')
+        return np.mean(np.delete(durations, [np.argmin(durations), np.argmax(durations)])).astype(int), operations
+
+    duration_avg, operations = get_duration_avg()
     allocator = Path(args[0]).name.replace('bench_', '')
     threads = args[1]
-    duration = retDurAvg
-    # operations = int(lines[1])
-    return allocator, threads, duration, operations
+    return allocator, threads, duration_avg, operations
 
 
 def main():
-
     subprocess.run(['make', 'clean'])
     subprocess.run(['make', '-j'])
     print(f'OPERATIONS: {OPERATIONS}')
@@ -88,10 +110,12 @@ def main():
                 if op[1] == 'Alloc' and bench == './build/bench_pmem':
                     continue  # do not run Alloc operation with bench_pmem, it is erroneous
                 for n in N_THREADS:
-                    allocator, threads, duration, operations = RepeatRun(
+                    allocator, threads, duration, operations = run_repeatedly(
                         [bench, n, op[0], op[1]])
                     f.write(f'{allocator},{threads},{duration},{operations}\n')
                     f.flush()
+    print(f'MAX_RUNS_CASES:')
+    print(f'{MAX_RUNS_CASES}')
 
 
 if __name__ == '__main__':
