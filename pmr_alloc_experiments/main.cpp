3fd88a9688c497bfc1aa2a54a797abc88dec7164
Myron Tsatsarakis <myronfirst@hotmail.com>
2024-08-12 21:41:32 +0300

    wip

--- pmr_alloc_experiments/main.cpp
+++ pmr_alloc_experiments/main.cpp
@@ -1,18 +1,22 @@
-// #include <cassert>
-#include <chrono>
-#include <iostream>
-// #include <string>
-// #include <thread>
-// #include <vector>
-
 #include <array>
+#include <cassert>
+#include <chrono>
 #include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <memory>
 #include <memory_resource>
+#include <thread>
+#include <vector>
 
 namespace {
-    // std::size_t LOGN_OPS = 7;
-    // std::size_t N_THREADS = std::thread::hardware_concurrency();
-    // std::size_t Nops;
+    constexpr std::size_t N_THREADS = 8;
+    constexpr std::size_t LOG2N_OPS = 25;
+    constexpr std::size_t ALLOC_SIZE = 16;
+    constexpr std::size_t ALLOC_BUFFER_SIZE = 1024 * 1024 * 1024;
+
+    auto constexpr Pow(std::size_t base, std::size_t exp) -> std::size_t { return exp == 0 ? 1 : base * Pow(base, exp - 1); }
+    constexpr std::size_t Nops = Pow(2, LOG2N_OPS);
 
     // [[maybe_unused]] auto IsPmem() -> bool {
     //     const std::string tmpPath = std::string{ Config::NVM_DIR } + "tmp_pool_tmp";
@@ -48,58 +52,74 @@ namespace {
     //     }
     // }
 
-    // auto PinThisThreadToCore(std::size_t core) -> void {
-    //     int err;
-    //     err = pthread_setconcurrency(static_cast<int>(std::thread::hardware_concurrency()));
-    //     if (err) {
-    //         std::cout << "pthread_setconcurrency error: " << strerror(errno) << "\n";
-    //         assert(false);
-    //         std::exit(EXIT_FAILURE);
-    //     }
-    //     cpu_set_t cpu_mask;
-    //     CPU_ZERO(&cpu_mask);
-    //     CPU_SET(core, &cpu_mask);
-    //     err = pthread_setaffinity_np(pthread_self(), sizeof(cpu_mask), &cpu_mask);
-    //     if (err) {
-    //         std::cout << "pthread_setaffinity_np error: " << strerror(errno) << "\n";
-    //         assert(false);
-    //         std::exit(EXIT_FAILURE);
-    //     }
-    // }
+    auto PinThisThreadToCore(std::size_t core) -> void {
+        int err;
+        err = pthread_setconcurrency(static_cast<int>(std::thread::hardware_concurrency()));
+        if (err) {
+            std::cout << "pthread_setconcurrency error: " << strerror(errno) << "\n";
+            assert(false);
+            std::exit(EXIT_FAILURE);
+        }
+        cpu_set_t cpu_mask;
+        CPU_ZERO(&cpu_mask);
+        CPU_SET(core, &cpu_mask);
+        err = pthread_setaffinity_np(pthread_self(), sizeof(cpu_mask), &cpu_mask);
+        if (err) {
+            std::cout << "pthread_setaffinity_np error: " << strerror(errno) << "\n";
+            assert(false);
+            std::exit(EXIT_FAILURE);
+        }
+    }
 
-    // auto AllocBenchmark() -> void {
-    //     using Clock = std::chrono::steady_clock;
-    //     using Millis = std::chrono::milliseconds;
-    //     std::size_t threadOps = Nops / N_THREADS;
-    //     std::vector<void*> allocs(Nops);
-    //     std::vector<std::thread> threads(N_THREADS);
-    //     // std::cout << "allocs capacity byte size: " << allocs.capacity() * sizeof(void*) << "\n";
-    //     const auto begin = Clock::now();
-    //     for (auto tid = 0u; tid < threads.size(); ++tid) {
-    //         const auto threadBegin = tid * threadOps;
-    //         const auto threadEnd = threadBegin + threadOps;
-    //         threads.at(tid) = std::thread([&allocs, tid, threadBegin, threadEnd]() {
-    //             PinThisThreadToCore(tid % N_THREADS);
-    //             for (auto i = threadBegin; i < threadEnd; ++i)
-    //                 allocs.at(i) = Interface::Alloc(Config::ALLOC_SIZE);
-    //         });
-    //     }
-    //     for (auto& t : threads) t.join();
-    //     const auto end = Clock::now();
-    //     std::cout << std::chrono::duration_cast<Millis>(end - begin).count() << "\n";
-    // }
-    constexpr std::size_t LOG2N_OPS = 25;
-    constexpr std::size_t ALLOC_SIZE = 16;
-    constexpr std::size_t ALLOC_BUFFER_SIZE = 1024 * 1024 * 1024;
+    auto Benchmark() -> void {
+        using Clock = std::chrono::steady_clock;
+        using Millis = std::chrono::milliseconds;
+        using TimePoint = Clock::time_point;
+        std::size_t threadOps = Nops / N_THREADS;
+        std::vector<std::thread> threads(N_THREADS);
+        TimePoint begin;
+        TimePoint end{};
+        Init();
+        for (auto tid = 0u; tid < threads.size(); ++tid) {
+            threads.at(tid) = std::thread([tid, threadOps, &begin, &end]() {
+                PinThisThreadToCore(tid % N_THREADS);
+                ThreadInit();
+                //BarrierWait
+                if (tid == 0) begin = Clock::now();
+                for (auto i = 0u; i < threadOps; ++i) Allocate(ALLOC_SIZE);
+                //BarrierWait
+                if (tid == 0) end = Clock::now();
+            });
+        }
+        for (auto& t : threads) t.join();
+        std::cout << std::chrono::duration_cast<Millis>(end - begin).count() << "\n";
+    }
 
-    auto constexpr Pow(std::size_t base, std::size_t exp) -> std::size_t { return exp == 0 ? 1 : base * Pow(base, exp - 1); }
-    constexpr std::size_t Nops = Pow(2, LOG2N_OPS);
+    class Allocator {
+    public:
+        virtual auto Init() -> void = 0;
+        virtual auto ThreadInit() -> void = 0;
+        virtual auto AllocateBytes(std::size_t) -> void* = 0;
+    };
+    class SyncPoolHeapAllocator {
+    private:
+    public:
+        virtual auto Init() -> void = 0;
+        virtual auto ThreadInit() -> void = 0;
+        virtual auto AllocateBytes(std::size_t) -> void* = 0;
+    };
 
     namespace SyncPoolHeap {
-        auto Init() -> void* {
+        std::unique_ptr<std::pmr::polymorphic_allocator> Allocator{};
+        auto Init() -> void {
+            std::pmr::set_default_resource(std::pmr::null_memory_resource());
             std::pmr::synchronized_pool_resource r{ std::pmr::new_delete_resource() };
+            std::pmr::polymorphic_allocator a{ &r };
+            Allocator = std::move(a);
         }
-        auto Allocate(std::size_t bytes) -> void*;
+        auto Allocate(std::size_t bytes) -> void* {
+            return Allocator->allocate_bytes(bytes);
+        }
     }    // namespace SyncPoolHeap
     namespace SyncPoolMBR {
         auto Init() -> void*;
