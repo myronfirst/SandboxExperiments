5af02f5db3b77c8aa5f0015e8e1e1bc9320db8c5
Myron Tsatsarakis <myronfirst@hotmail.com>
2024-08-26 15:16:53 +0300

    hotspot export

--- pmr_alloc_experiments/hotspot.sh
+++ pmr_alloc_experiments/hotspot.sh
@@ -3,4 +3,6 @@
 make clean
 make all
 sudo perf record --call-graph dwarf build/main
-sudo hotspot
+sudo chmod 777 perf.data
+sudo hotspot --exportTo hotspot_results.perfparser perf.data
+# sudo hotspot
